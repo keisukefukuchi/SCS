3689e998b8cf722c043306d0c12009a0d5d23dd5
terata53 <al19035@shibaura-it.ac.jp>
2021-06-22 14:46:47 +0900

    model,controllerの追加

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -3,8 +3,162 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Message;
+use App\Models\Follower;
+use App\Models\Channel;
+use App\Models\Join;
 
 class MessagesController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request, Message $message, Follower $follower, Channel $channel, Join $join)
+    {
+        $user = auth()->user();
+        $follow_ids = $follower->followingIds($user->id);
+        // followed_idだけ抜き出す
+        $following_ids = $follow_ids->pluck('followed_id')->toArray();
+
+        $channel_id = $request->input('channel_id');
+        if (empty($channel_id)){
+            $channel_id = 1;
+        }
+
+        $timelines = $message->getTimelines($user->id, $following_ids, $channel_id);
+
+        $join_channels = $join->joinChannels($user->id);
+        $join = $join_channels->pluck('channel_id')->toArray();
+
+        $channels = $channel->getChannels($join);
+        $channel_name = $channel->getChannelName($channel_id);
+
+        return view('messages.index', [
+            'user'      => $user,
+            'reply_id'  => 0,
+            'timelines' => $timelines,
+            'channels'  => $channels,
+            'channel_id'  => $channel_id,
+            'channel_name' => $channel_name
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request, Channel $channel, Message $message)
+    {
+        $user = auth()->user();
+        $reply_id = $request->input('reply_id');
+        $channel_id = $request->input('channel_id');
+        //$channel_id = 1;
+
+        $channel_name = $channel->getChannelName($channel_id);
+
+        return view('messages.create', [
+            'user' => $user,
+            'reply_id' => $reply_id,
+            'channel_name' => $channel_name,
+            'channel_id' => $channel_id,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Message $message)
+    {
+        $user = auth()->user();
+        $data = $request->all();
+        $validator = Validator::make($data, [
+            'message' => ['required', 'string', 'max:140']
+        ]);
+
+        $validator->validate();
+        $message->messageStore($user->id, $data);
+
+        return redirect('messages');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Message $message)
+    {
+        $user = auth()->user();
+        $message = $message->getMessage($message->id);
+        $reply = $message->getMessage($message->reply_id);
+
+        return view('messages.show', [
+            'user' => $user,
+            'message' => $message,
+            'reply' => $reply,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Message $message)
+    {
+        $user = auth()->user();
+        $messages = $message->getEditMessage($user->id, $message->id);
+
+        if (!isset($messages)) {
+            return redirect('messages');
+        }
+
+        return view('messages.edit', [
+            'user'   => $user,
+            'messages' => $messages
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Message $message)
+    {
+        $data = $request->all();
+        $validator = Validator::make($data, [
+            'message' => ['required', 'string', 'max:140']
+        ]);
+
+        $validator->validate();
+        $message->messageUpdate($message->id, $data);
+
+        return redirect('messages');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Message $message)
+    {
+        $user = auth()->user();
+        $message->messageDestroy($user->id, $message->id);
+
+        return redirect('messages');
+    }
 }
--- app/Models/Message.php
+++ app/Models/Message.php
@@ -2,10 +2,107 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Message extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assingnable.
+     *
+     * @var array
+     */
+
+    protected $fillable = [
+       'message'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function favorites()
+    {
+        return $this->hasMany(Favorite::class);
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(message::class);
+    }
+
+    public function getUserTimeLine(Int $user_id)
+    {
+        return $this->where('user_id', $user_id)->orderBy('created_at', 'DESC')->paginate(50);
+    }
+
+    public function getMessageCount(Int $user_id)
+    {
+        return $this->where('user_id', $user_id)->count();
+    }
+
+    //指定ユーザIDとメッセージID(id)に合致する返信件数を取得する
+    public function getReplyCount(Int $user_id, Int $message_id)
+    {
+        return $this->where('user_id', $user_id)->where('reply_id', $message_id)->count();
+    }
+    
+    //指定ユーザIDとメッセージID(id)に合致する返信テーブルを取得する
+    public function getReply(Int $user_id, Int $message_id)
+    {
+        return $this->where('user_id', $user_id)->where('reply_id', $message_id);
+    }
+
+     // 一覧画面
+    public function getTimeLines(Int $user_id, Array $follow_ids, Int $channel_id)
+    {
+        // 自身とフォローしているユーザIDを結合する
+        $follow_ids[] = $user_id;
+        return $this->whereIn('user_id', $follow_ids)->where('channel_id', $channel_id)->orderBy('created_at', 'DESC')->paginate(50);
+    }
+
+     // 詳細画面
+    public function getMessage(Int $message_id)
+    {
+        return $this->with('user')->where('id', $message_id)->first();
+    }
+
+    public function messageStore(Int $user_id, Array $data)
+    {
+        $this->user_id = $user_id;
+        $this->message = $data['message'];
+        //$this->reply_id = $data['reply_id'];
+        $this->reply_id = 1;
+        $this->channel_id = $data['channel_id'];
+        $this->save();
+
+        return;
+    }
+
+    public function getEditMessage(Int $user_id, Int $message_id)
+    {
+        return $this->where('user_id', $user_id)->where('id', $message_id)->first();
+    }
+
+    public function messageUpdate(Int $message_id, Array $data)
+    {
+        $this->id = $message_id;
+        $this->message = $data['message'];
+        $this->update();
+
+        return;
+    }
+
+    public function messageDestroy(Int $user_id, Int $message_id)
+    {
+        return $this->where('user_id', $user_id)->where('id', $message_id)->delete();
+    }
+
+    public function messageSearch(string $keyword)
+    {
+        return $this->where('message', 'like', '%'.$keyword.'%')->orderBy('created_at', 'DESC')->paginate(50);
+    }
 }
